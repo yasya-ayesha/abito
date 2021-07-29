47b0dd9b6bf06c61bbad906bd01a826baebbdcd1
Yasya <80125891+yasya-ayesha@users.noreply.github.com>
2021-07-29 16:21:45 +0300

    Add files via upload
    realised adaptive

--- js/main.js
+++ js/main.js
@@ -11,3 +11,13 @@ var swiper = new Swiper(".mySwiper", {
       swiper: swiper,
     },
   });
+  
+  const menuBtn = document.querySelector('.menu-button');
+  const menu = document.querySelector('.navbar-panel');
+  const menuCloseBtn = document.querySelector('.close-menu');
+  menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('is-open');
+  });
+  menuCloseBtn.addEventListener('click', () => {
+    menu.classList.toggle('is-open');
+  })
