fb65275407e286223b1cb996286fa98c480dd988
Yasya <80125891+yasya-ayesha@users.noreply.github.com>
2021-07-29 17:03:10 +0300

    Update main.js
    scroll lock

--- js/main.js
+++ js/main.js
@@ -1,23 +1,46 @@
-var swiper = new Swiper(".mySwiper", {
+const swiper = new Swiper(".mySwiper", {
     spaceBetween: 10,
     slidesPerView: 6,
     freeMode: true,
     watchSlidesVisibility: true,
     watchSlidesProgress: true,
-  });
-  var swiper2 = new Swiper(".mySwiper2", {
+});
+const swiper2 = new Swiper(".mySwiper2", {
     spaceBetween: 10,
     thumbs: {
       swiper: swiper,
     },
-  });
+});
   
-  const menuBtn = document.querySelector('.menu-button');
-  const menu = document.querySelector('.navbar-panel');
-  const menuCloseBtn = document.querySelector('.close-menu');
-  menuBtn.addEventListener('click', () => {
+const menuBtn = document.querySelector('.menu-button');
+const menu = document.querySelector('.navbar-panel');
+const menuCloseBtn = document.querySelector('.close-menu');
+const disableScroll = () => {
+    const widthScroll = window.innerWidth - document.body.offsetWidth;
+    document.body.dbScrollY = window.scrollY;
+    document.body.style.cssText = `
+    position: fixed;
+    top: ${-window.scrollY}px;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    padding-right: ${widthScroll}px;
+    `;
+};
+const enableScroll = () => {
+    document.body.style.cssText = '';
+    window.scroll({
+        top: document.body.dbScrollY,
+    });
+};
+
+menuBtn.addEventListener('click', () => {
     menu.classList.toggle('is-open');
-  });
-  menuCloseBtn.addEventListener('click', () => {
+    disableScroll();
+});
+menuCloseBtn.addEventListener('click', () => {
     menu.classList.toggle('is-open');
-  })
+    enableScroll();
+});
+
